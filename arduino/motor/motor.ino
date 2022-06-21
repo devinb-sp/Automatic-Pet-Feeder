fc10c02b21a9414bcc85735def4aeb02c986d8ef
Jose Tapizquent <josetapizq@gmail.com>
2022-06-20 21:21:22 -0400

    CU-2mrnbkr - DC Motor code update

--- arduino/motor/motor.ino
+++ arduino/motor/motor.ino
@@ -1,10 +1,18 @@
-#define MOTOR_PIN 3
-#define ON_MOTOR_SPEED 100
+#define MOTOR_PIN 9
+#define MOTOR_IN_1 8
+#define MOTOR_IN_2 7
+#define ON_MOTOR_SPEED 255
 #define OFF_MOTOR_SPEED 0
+
+// Actions
+#define START_MOTOR_ACTION 1
+#define STOP_MOTOR_ACTION 0
+
+#define 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(MOTOR_PIN, OUTPUT);
+  setUpMotor();
 }
 
 void loop() {
@@ -21,15 +29,35 @@ void loop() {
 
     switch(action)
     {
-      case 0:
-        analogWrite(MOTOR_PIN, OFF_MOTOR_SPEED);
+      case STOP_MOTOR_ACTION:
+        stopMotorAction();
         break;
-      case 1:
-        analogWrite(MOTOR_PIN, ON_MOTOR_SPEED);
+      case START_MOTOR_ACTION:
+        startMotorAction();
         break;
       default:
         break;
     }
   }
+}
 
+void setUpMotor()
+{
+  pinMode(MOTOR_PIN, OUTPUT);
+  digitalWrite(MOTOR_IN_1, LOW);
+  digitalWrite(MOTOR_IN_2, LOW);
+}
+
+void startMotorAction()
+{
+  analogWrite(MOTOR_PIN, ON_MOTOR_SPEED);
+  digitalWrite(MOTOR_IN_1, HIGH);
+  digitalWrite(MOTOR_IN_2, LOW);
+}
+
+void stopMotorAction()
+{
+  analogWrite(MOTOR_PIN, OFF_MOTOR_SPEED);
+  digitalWrite(MOTOR_IN_1, LOW);
+  digitalWrite(MOTOR_IN_2, LOW);
 }
